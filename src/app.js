fec36ea44fdd1a6885163ee561f117450431bfd8
jjmartel <juanomartel@gmail.com>
2025-03-14 20:27:00 +0000

    basico

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  let pronoun = ['the', 'our', 'my'];
+  let adj = ['great', 'big','small','fast'];
+  let noun = ['jogger', 'racoon','neandhertal','magician'];
+  let domain = ['.es','.com','.org','.ar']
+
+  for (let i in pronoun){
+    for (let j in adj) {
+      for(let k in noun){
+        for (let l in domain){
+          const arrayList = pronoun[i] + adj[j] + noun[k] + domain[l]
+          console.log(arrayList)
+        }
+      }
+    }
+  }
+  
+  
+
 };
